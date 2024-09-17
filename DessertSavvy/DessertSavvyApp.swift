7081061de7ebe4323f85dffd97abb5de90f18d6b
Nathan Hramits <nhramits@gmail.com>
2024-09-17 15:13:43 -0700

    Initial Commit

---
+++ DessertSavvy/DessertSavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertSavvyApp.swift
+//  DessertSavvy
+//
+//  Created by Nathan Hramits on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertSavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
